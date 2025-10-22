7e7cd22a8684bd7336063836bf239be9d12976a0
TheMonHub <mono15.parnthakote@gmail.com>
2025-10-22 19:51:38 +0700

    Remove redundant compiler flags from clang-tidy scripts
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>

--- automated/clang-tidy-fix.sh
+++ automated/clang-tidy-fix.sh
@@ -2,6 +2,6 @@
 
 cd "$(dirname "$0")"
 mkdir -p clang_tidy_build
-cmake -S .. -B clang_tidy_build -DCMAKE_C_COMPILER="$(which clang)" -DCMAKE_CXX_COMPILER="$(which clang++)" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
+cmake -S .. -B clang_tidy_build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
 cmake --build clang_tidy_build
 run-clang-tidy '^(?!.*(\.asm|\.asm.inc|_deps/))' -p clang_tidy_build -quiet -fix
