cb9a5d57e85fdd27e74ed9c26f56cb9ea2061056
TheMonHub <mono15.parnthakote@gmail.com>
2025-10-24 23:53:52 +0700

    Add SPDX license headers and add an exception
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>

--- include/dummy.hpp
+++ include/dummy.hpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 TheMonHub
+//
+// SPDX-License-Identifier: LicenseRef-Apache-2.0-NoModNotice
+
 #ifndef AKL_DUMMY_HPP
 #define AKL_DUMMY_HPP
 #endif  // AKL_DUMMY_HPP
--- src/dummy.cpp
+++ src/dummy.cpp
@@ -1,3 +1,5 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2025 TheMonHub
+//
+// SPDX-License-Identifier: LicenseRef-Apache-2.0-NoModNotice
 
 int main() {}
