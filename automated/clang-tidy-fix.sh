3ca705f801a10ac9908012692b78c955dfbec9f4
TheMonHub <mono15.parnthakote@gmail.com>
2025-10-31 20:51:39 +0700

    Feature/init project (#9)
    * Remove redundant compiler flags from clang-tidy scripts
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Add workflow_dispatch trigger to lint workflow
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Update project's structure
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Add line ending settings for additional file types in .gitattributes
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Initialize project with CMake configuration and add user report template
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Add Code of Conduct and update issue templates for clarity
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Add SECURITY.md to outline vulnerability reporting process
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Add initial content to CONTRIBUTING.md
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Update project description in CMakeLists.txt and revise response times in SECURITY.md
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * fix format
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * remove unsupported config
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * clang-tidy fix
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Add SPDX license headers and add an exception
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Refine Modification Notice Exception in LICENSE
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Update SECURITY.md
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Rename dummy.cpp to main.cpp
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Change SPDX license headers in scripts and source(s)
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Add contributing guidelines and update project description in README
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Update CONTRIBUTING.md for clarity and requirements
    
    Clarified instructions for internal contributors and updated file header requirements.
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Test if it's needed
    
    Comment out the build command in clang-tidy script
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Apparently not needed
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Finish the doc
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Adjust wording
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * Update project configuration and documentation files
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * test
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * test again
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    * convert clang format config to use compatible value
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>
    
    ---------
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>

--- automated/clang-tidy-fix.sh
+++ automated/clang-tidy-fix.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")"/..
 mkdir -p clang_tidy_build
-cmake -S .. -B clang_tidy_build -DCMAKE_C_COMPILER="$(which clang)" -DCMAKE_CXX_COMPILER="$(which clang++)" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
-cmake --build clang_tidy_build
-run-clang-tidy '^(?!.*(\.asm|\.asm.inc|_deps/))' -p clang_tidy_build -quiet -fix
+cmake -S . -B clang_tidy_build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
+run-clang-tidy '^(?!.*(\.asm|\.asm.inc|_deps/))' -p "$(dirname "$0")"/clang_tidy_build -quiet -fix
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+/*
+ * File: main.cpp
+ * Description: Entry point for the process.
+ *
+ * Copyright (C) 2025 TheMonHub
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int main() {}
