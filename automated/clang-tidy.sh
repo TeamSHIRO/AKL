6cb3b1f69bb59c44994884d310ce3fb313fda64a
TheMonHub <mono15.parnthakote@gmail.com>
2025-10-31 11:07:43 +0700

    Test if it's needed
    Comment out the build command in clang-tidy script
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>

--- automated/clang-tidy.sh
+++ automated/clang-tidy.sh
@@ -3,5 +3,5 @@
 cd "$(dirname "$0")"
 mkdir -p clang_tidy_build
 cmake -S .. -B clang_tidy_build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
-cmake --build clang_tidy_build
+# cmake --build clang_tidy_build
 run-clang-tidy '^(?!.*(\.asm|\.asm.inc|_deps/))' -p clang_tidy_build -quiet
