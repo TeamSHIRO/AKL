b12ea22bb48c98ad5c601e11a23ad44ba69ca58c
TheMonHub <mono15.parnthakote@gmail.com>
2025-11-02 16:21:40 +0700

    Update clang-tidy.sh
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>

--- automated/clang-tidy.sh
+++ automated/clang-tidy.sh
@@ -2,5 +2,5 @@
 
 cd "$(dirname "$0")"/..
 mkdir -p "$(dirname "$0")"/clang_tidy_build
-cmake -S . -B "$(dirname "$0")"/clang_tidy_build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
+cmake -S . -B "$(dirname "$0")"/clang_tidy_build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 run-clang-tidy '^(?!.*_deps/)(?=.*\.c(pp)?).*' -p "$(dirname "$0")"/clang_tidy_build -quiet
